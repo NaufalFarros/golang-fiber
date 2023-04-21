f01e27b3be424253ec020c14339a806e62b15a40
Naufal Farros <71809599+NaufalFarros@users.noreply.github.com>
2023-04-21 10:32:30 +0700

    Update

--- database/database.go
+++ database/database.go
@@ -17,7 +17,7 @@ type DbInstance struct {
 var Database DbInstance
 
 func Connect() {
-	dsn := "host=database-2.cgl7qm7eiaf0.ap-southeast-1.rds.amazonaws.com user=postgres password=GodhandLND356 dbname=farosRDS port=5432 sslmode=enable TimeZone=Asia/Jakarta"
+	dsn := "host=database-2.cgl7qm7eiaf0.ap-southeast-1.rds.amazonaws.com user=postgres password=GodhandLND356 dbname=farosRDS port=5432 sslmode=require TimeZone=Asia/Jakarta"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error connecting to database", err.Error())
